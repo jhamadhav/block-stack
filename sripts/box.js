78b232abc287473a87c6baaafe548ba33269fc48
jhamadhav <jhamadhav28@gmail.com>
2020-02-12 13:12:01 +0530

    initial build

---
+++ sripts/box.js
@@ -0,0 +1,27 @@
+function Box(x, y, W, H = box_h) {
+    this.x = x;
+    this.y = y;
+    this.w = W;
+    this.h = H;
+    this.m = false;
+
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.strokeStyle = '#ccc';
+        ctx.lineWidth = 0.6;
+        ctx.fillStyle = clr;
+        ctx.rect(this.x, this.y, this.w, this.h);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    this.move = function () {
+        if (this.m) {
+            this.x += v;
+            if (this.x > w + this.w || this.x < -this.w) {
+                v = -v;
+            }
+        }
+    }
+}
